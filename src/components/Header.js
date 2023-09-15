e38a3f428e3d7446a7d111023fd8b8c971f020c5
Ivan Hancko <ivanhancko@yahoo.com>
2023-09-15 12:18:25 +0200

    create header and meme component

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from './images/trollFace.png'
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img className='meme--logo' src= {logo} alt='' />
+            <h2 className='meme--heading'>Meme Generator</h2>
+            <h4 className='meme--title'>React Course - Project 3</h4>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/Meme.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Meme = () => {
+  return (
+    <section>
+        <form>
+            <input type="text" />
+            <input type="text" />
+            <button type="button">Submit</button>
+        </form>
+    </section>
+  )
+}
+
+export default Meme
